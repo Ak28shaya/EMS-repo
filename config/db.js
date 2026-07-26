f9946d35fa4c11d87346bc1db13b2c4297bfaecf
Sathya <sathya.m2023@vitstudent.ac.in>
2026-07-26 20:55:02 +0530

    modifies in connection string

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/EMS");
+        await mongoose.connect("mongodb://sathya_db_user:Sathya12345@ac-vegkojf-shard-00-00.geospr9.mongodb.net:27017,ac-vegkojf-shard-00-01.geospr9.mongodb.net:27017,ac-vegkojf-shard-00-02.geospr9.mongodb.net:27017/?ssl=true&replicaSet=atlas-18kfn7-shard-0&authSource=admin&appName=SkyWingsCluster");
         console.log("MongoDB Connected");
     } catch (error) {
         console.log("Database Connection Failed");
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
 });
-// Routes
+
 app.use("/auth", authRoutes);
 // Start Server
 const PORT = 5000;
